a3423a4f8798fa31b588990a9e117772911a9fc8
Shahrulhadi <shahrulhadi@gmail.com>
2025-01-12 17:08:05 +0800

    add another image

--- index.php
+++ index.php
@@ -9,10 +9,11 @@
 <body>
 
   <h1>Hello world!</h1>
-  <div><img src="gundam1.png" alt="gundam"
+  <div><img src="gundam1.jpg" alt="gundam"
       style="width:100%;max-width:960px"></div>
   <p>This is the first file in my new Git Repo.</p>
   <p>A new line in our file!</p>
+  <div><img src="pagani-zonda.jpg" alt="Hello Git" style="width:100%;max-width:640px"></div>
 
 </body>
 
