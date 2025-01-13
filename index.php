d807993f8c879398c03f04caceba3018fb48aa02
Shahrulhadi <Shahrulhadi@gmail.com>
2025-01-13 13:25:30 +0800

    Update index.php
    Tamboh header h5 untuk gambo.
    Buang line yg dok perlu.

--- index.php
+++ index.php
@@ -8,12 +8,13 @@
 
 <body>
 
-  <h1>Hello world!</h1>
+  <h1>Tutorial Git Hello World</h1>
+  <h5>Gambo gundam</h5>
   <div><img src="gundam1.png" alt="gundam"
       style="width:100%;max-width:800px"></div>
   <p>This is the first file in my new Git Repo.</p>
-  <p>A new line in our file!</p>
   <p>This line is here to show how merging works.</p>
+  <h5>Gambo pagani zonda</h5>
   <div><img src="pagani-zonda.jpg" alt="Hello Git" style="width:100%;max-width:640px"></div>
 
 </body>
